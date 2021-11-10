5823fa049ea4daf15975e678dec59c9b76c5353d
lobo83 <inmortalland83@gmail.com>
2021-11-10 08:38:39 +0100

    juego con el shadow dom

--- src/app/projection/projection.component.ts
+++ src/app/projection/projection.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-projection',
   templateUrl: './projection.component.html',
-  styleUrls: ['./projection.component.scss']
+  styleUrls: ['./projection.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ProjectionComponent implements OnInit {
 
