dfd4af01894e6c3f85be0200f09421d3301917e3
lobo83 <inmortalland83@gmail.com>
2021-11-23 10:12:12 +0100

    añadida cierta mejora para que al cargar la pagina elimine de las sugerencias el item mostrado. arreglar para que se refresque las sugerencias con cada click

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,18 +9,35 @@ import { Product, ProductService } from '../shared/services/product.service';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent  {
+export class ProductComponent {
 
   product$: Observable<Product>;
-  suggestedProducts$: Observable<Product[]>;
+  suggestedProducts$: Observable<Product[]> ;
 
-  constructor(private route: ActivatedRoute, private productService: ProductService) { 
-    this.product$=this.route.paramMap.pipe(map(params => parseInt(params.get('productId')||'',10)),
-    filter(productId=>!!productId),
-    switchMap(productId=>this.productService.getById(productId))
+  constructor(private route: ActivatedRoute, private productService: ProductService) {
+    let prodId = 0;
+    this.product$ = this.route.paramMap.pipe(map(params => parseInt(params.get('productId') || '', 10)),
+      filter(productId => !!productId),
+      switchMap(productId => {
+
+        prodId = productId;
+        return this.productService.getById(productId);
+      }
+      )
     );
-    this.suggestedProducts$=this.productService.getAll();
-  }
+    this.suggestedProducts$=this.productService.getAll().pipe(map(products=>{
+      return products.filter(product=>product.id !== prodId);
+    }));
+     /*this.productService.getAll().subscribe(products => {
+      
+      for (let product of products){
+        if (product.id !== prodId){
+          this.suggestedProducts.push(product);
+        }
+    }
+  });*/
+  
+}
 
   
 
